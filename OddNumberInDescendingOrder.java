e2bb9fd334597c0005875b7b4e5cfe1407021f29
PraiseJah-Graybot <edwardpraisejah@gmail.com>
2025-04-02 15:59:24 +0100

    Looping Construct

---
+++ OddNumberInDescendingOrder.java
@@ -0,0 +1,16 @@
+public class OddNumberInDescendingOrder
+{
+	public static void main(String[] args)
+	{
+		int i = 50;
+		
+		while(i >= 1)
+		{
+			if(i%2 == 1)
+			{
+				System.out.printf("%d%n",i);
+			}
+			i--;
+		}
+	}
+}
